1d02a60807b7b4a39f34c4a43f847ba508e32383
brian <mairurat@gmail.com>
2023-03-11 00:02:03 +0300

    build up portifolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./home.css";
+import { FaFigma } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { CiTwitter } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home_container">
       <div className="home_page">
+        <div className="gradient1" />
         <div className="header">
           <div className="details">
             <h3>Loupa</h3>
@@ -14,11 +20,34 @@ const Home = () => {
               <li>Blog</li>
             </ul>
           </div>
+          <div className="gradient2" />
           <div className="socials">
-            <p>IG</p>
-            <p>Twitter</p>
-            <p>Whatsapp</p>
+            <ul>
+              <li>
+                <a href="#" alt="figmaicon">
+                  <FaFigma />{" "}
+                </a>
+              </li>
+              <li>
+                <a href="#" alt="twitter">
+                  <FaInstagram />{" "}
+                </a>
+              </li>
+              <li>
+                <a href="#" alt="ig">
+                  <CiTwitter />{" "}
+                </a>
+              </li>
+              <li>
+                {" "}
+                <a href="#" alt="ig">
+                  <FaWhatsapp />{" "}
+                </a>
+              </li>
+            </ul>
           </div>
+          <div className="gradient3" />
+          <div className="gradient4" />
         </div>
       </div>
     </div>
